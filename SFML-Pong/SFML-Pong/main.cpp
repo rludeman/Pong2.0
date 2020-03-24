4c02fa0fea1c362277d4d238d00b7a75b3c70311
Ryan Ludeman <the.rluda@gmail.com>
2020-03-24 13:48:18 -0500

    Create Start Screen
    Created simple start screen with minimal functionality.

--- SFML-Pong/SFML-Pong/main.cpp
+++ SFML-Pong/SFML-Pong/main.cpp
@@ -13,9 +13,37 @@ int main()
 		std::cout << "Failed to load font." << std::endl;
 		return 1;
 	}
-	sf::Text msg;
-	msg.setFont(font);
-	msg.setString("Hello Pong!");
+
+	// Start Screen
+	sf::Text title;
+	title.setFont(font);
+	title.setCharacterSize(50);
+	title.setPosition(10, 10);
+	title.setString("SFML-Pong!");
+
+	// Play Button
+	sf::RectangleShape playButton(sf::Vector2f(150, 50));
+	playButton.setPosition(10, 70);
+	playButton.setFillColor(sf::Color::Black);
+	playButton.setOutlineThickness(2);
+
+	sf::Text playButtonMsg;
+	playButtonMsg.setFont(font);
+	playButtonMsg.setCharacterSize(40);
+	playButtonMsg.setPosition(10, 70);
+	playButtonMsg.setString("Play");
+
+	// Quit Button
+	sf::RectangleShape quitButton(sf::Vector2f(150, 50));
+	quitButton.setPosition(10, 130);
+	quitButton.setFillColor(sf::Color::Black);
+	quitButton.setOutlineThickness(2);
+
+	sf::Text quitButtonMsg;
+	quitButtonMsg.setFont(font);
+	quitButtonMsg.setCharacterSize(40);
+	quitButtonMsg.setPosition(10, 130);
+	quitButtonMsg.setString("Quit");
 	
 	sf::Event event;
 
@@ -25,10 +53,25 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+			if (event.type == sf::Event::MouseButtonPressed && 
+				event.mouseButton.button == sf::Mouse::Button::Left)
+			{
+				sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+				if (playButton.getGlobalBounds().contains(mousePos))
+					std::cout << "Play Game!" << std::endl;
+				if (quitButton.getGlobalBounds().contains(mousePos))
+					window.close();
+			}
 		}
 
 		window.clear();
-		window.draw(msg);
+
+		window.draw(title);
+		window.draw(playButton);
+		window.draw(playButtonMsg);
+		window.draw(quitButton);
+		window.draw(quitButtonMsg);
+
 		window.display();
 	}
 
