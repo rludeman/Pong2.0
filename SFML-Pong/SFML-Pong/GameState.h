1d723b30e2840812dcb1f4145df388ae6c7eba58
Ryan Ludeman <the.rluda@gmail.com>
2020-03-24 19:02:08 -0500

    Created Start Screen Class
    Moved start screen components to seperate class.  Modified event and game loops.  Added header file for GameState enum.

---
+++ SFML-Pong/SFML-Pong/GameState.h
@@ -0,0 +1,3 @@
+#pragma once
+
+enum GameState { Menu, Game };
---
+++ SFML-Pong/SFML-Pong/StartScreen.cpp
@@ -0,0 +1,59 @@
+#include "StartScreen.h"
+
+StartScreen::StartScreen(const sf::Font& font)
+{
+	// Title
+	title.setFont(font);
+	title.setCharacterSize(50);
+	title.setPosition(10, 10);
+	title.setString("SFML-Pong!");
+
+	// Play Button
+	playButton.box.setSize(sf::Vector2f(150, 50));
+	playButton.box.setPosition(10, 70);
+	playButton.box.setFillColor(sf::Color::Black);
+	playButton.box.setOutlineThickness(2);
+	playButton.msg.setFont(font);
+	playButton.msg.setCharacterSize(40);
+	playButton.msg.setPosition(10, 70);
+	playButton.msg.setString("Play");
+
+	// Quit Button
+	quitButton.box.setSize(sf::Vector2f(150, 50));
+	quitButton.box.setPosition(10, 130);
+	quitButton.box.setFillColor(sf::Color::Black);
+	quitButton.box.setOutlineThickness(2);
+	quitButton.msg.setFont(font);
+	quitButton.msg.setCharacterSize(40);
+	quitButton.msg.setPosition(10, 130);
+	quitButton.msg.setString("Quit");
+}
+
+
+StartScreen::~StartScreen()
+{
+}
+
+void StartScreen::handleEvents(const sf::Event& event, sf::RenderWindow& window, GameState& currentState)
+{
+	if (event.type == sf::Event::MouseButtonPressed)
+	{
+		if (event.mouseButton.button == sf::Mouse::Button::Left)
+		{
+			sf::Vector2f mousePos = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
+			if (playButton.box.getGlobalBounds().contains(mousePos))
+				currentState = Game;
+			if (quitButton.box.getGlobalBounds().contains(mousePos))
+				window.close();
+		}
+	}
+}
+
+void StartScreen::draw(sf::RenderWindow& window)
+{
+	window.draw(title);
+	window.draw(playButton.box);
+	window.draw(playButton.msg);
+	window.draw(quitButton.box);
+	window.draw(quitButton.msg);
+}
