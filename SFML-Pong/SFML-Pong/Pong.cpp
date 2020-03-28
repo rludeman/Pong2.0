0e1610d48a9c7408fb1534a91af3d42df193530f
Ryan Ludeman <the.rluda@gmail.com>
2020-03-28 00:11:51 -0500

    Add Pong.cpp changes to commits
    Accidentally unstaged this file for the last 2 commits.... be careful out there....

--- SFML-Pong/SFML-Pong/Pong.cpp
+++ SFML-Pong/SFML-Pong/Pong.cpp
@@ -2,29 +2,10 @@
 #include "Pong.h"
 
 
-Pong::Pong(const sf::Font* fontPtr, const sf::RenderWindow& window) : currentState(Menu), fontPtr(fontPtr) // TODO Improve game-state and global object management
+Pong::Pong(const sf::Font* fontPtr, const sf::RenderWindow& window) : currentState(Menu)	
 {
-	menu.setFont(*fontPtr);
-
-	// TODO move default initialization to constructors and add setters/getters
-	// Paddles
-	paddleA.setSize(sf::Vector2f(10, 100)); 
-	paddleB.setSize(sf::Vector2f(10, 100));
-	paddleA.setPosition(10.f, window.getSize().y / 2.f - 50.f);
-	paddleB.setPosition(window.getSize().x - 10.f, window.getSize().y / 2.f - 50.f);
-
-	// Ball
-	ball.setRadius(10.f);
-	ball.setOrigin(ball.getRadius(), ball.getRadius());
-	ball.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
-
-	// AI Controller
-	ai.setPaddle(&paddleB);
-
-	// Winner Message
-	winMsg.setFont(*fontPtr);
-	winMsg.setCharacterSize(100);
-	winMsg.setPosition(200, 200);
+	menu.setFont(*fontPtr);	// TODO handle fonts/assets more uniformly
+	game.init(fontPtr, window);
 }
 
 Pong::~Pong()
@@ -36,51 +17,17 @@ void Pong::handleEvents(const sf::Event & event, sf::RenderWindow & window)
 	switch (currentState)
 	{
 	case Menu:
-		menu.handleEvents(event, window, currentState);
+		menu.handleEvents(event, window);
 		break;
 
 	case Game:
-		if (event.type == sf::Event::KeyPressed)
-		{
-			switch (event.key.code)
-			{
-			case sf::Keyboard::Space:
-				ball.setVelocity(sf::Vector2f(-Ball::DefaultSpeed, Ball::DefaultSpeed)); // TODO disable after launch
-				break;
-
-			case sf::Keyboard::W:
-				paddleA.moveUp();
-				break;
-
-			case sf::Keyboard::S:
-				paddleA.moveDown();
-				break;
-
-			case sf::Keyboard::Escape:
-				currentState = Menu;
-				reset(window);
-				break;
-			}
-		}
-		else if (event.type == sf::Event::KeyReleased)
-		{
-			switch (event.key.code)
-			{
-			case sf::Keyboard::W:
-				paddleA.moveDown();
-				break;
-
-			case sf::Keyboard::S:
-				paddleA.moveUp();
+		game.handleEvents(event, window);
 		break;
 	}
 }
 
-void Pong::update(sf::Time deltaTime, const sf::RenderWindow & window) // TODO implement update queue to handle physics/collisions
+void Pong::update(sf::Time deltaTime, const sf::RenderWindow& window) // TODO implement physics/collisions
 {
-	ball.update(deltaTime, window, paddleA, paddleB);
-	ai.udpate(ball);
-	paddleA.update(deltaTime, window);
 	game.update(deltaTime, window);
 }
 
@@ -93,43 +40,6 @@ void Pong::draw(sf::RenderWindow & window) // TODO make components 'drawable' so
 		break;
 
 	case Game:
-		sf::Text score(std::to_string(scoreA) + " | " + std::to_string(scoreB), *fontPtr, 40);
-		window.draw(score);
-		window.draw(ball);
-		window.draw(paddleA);
-		window.draw(paddleB);
-		if (scoreA >= 5)
-		{
-			winMsg.setString("You Win!"); // TODO End and reset game (score) with winning
-			window.draw(winMsg);
-		}
-		if (scoreB >= 5)
-		{
-			winMsg.setString("You Lose");
-			window.draw(winMsg);
-		}
-	}
-}
-
-void Pong::reset(const sf::RenderWindow& window)
-{
-	paddleA.setPosition(10.f, window.getSize().y / 2.f - 50.f);
-	paddleB.setPosition(window.getSize().x - 10.f, window.getSize().y / 2.f - 50.f);
-	ball.setVelocity(sf::Vector2f(0.f, 0.f));
-	ball.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
-}
-
-GameState Pong::getState() { return currentState; }
-
-bool Pong::checkScore(const Ball& ball, const sf::RenderWindow& window)
-{
-	if (ball.getPosition().x < 0.f)
-	{
-		++scoreB;
-		return true;
-	}
-	if (ball.getPosition().x > window.getSize().x)
-		++scoreA;
 		game.draw(window);
 		break;
 	}
